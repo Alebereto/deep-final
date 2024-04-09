6379bd8d717b0ba155f8b29a59b3f37c95d6eab3
Alebereto <1albertkhayat1@gmail.com>
2024-04-09 22:11:32 +0300

    Add logger functions

--- model/logger.py
+++ model/logger.py
@@ -1,15 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import deque
+import os
+from data.ImagesDataset import tensor_to_image, gray_tensor_to_image
+
+SAVE_PATH = "results"
 
 class Logger():
 	""" Class containing information of model, such as layer dimensions
 	 	or training stats. Also has functions for plots and debugging. """
 
-	def __init__(self, g_params, d_params) -> None:
+	def __init__(self, name, hyparams, g_params, d_params) -> None:
+		self.name = name
+		self.hyparams = hyparams
 		self.g_params = g_params
 		self.d_params = d_params
 
-		self.train_loss = []
+		self.g_test_loss, self.g_train_loss = list(), list()
+		self.d_test_loss, self.d_train_loss = list(), list()
+
+		self.recent_images = deque(maxlen=5) # tuples of (gray, colored, fake) as tensors
 		
 		self.epochs_trained = 0
 
@@ -19,39 +29,72 @@ class Logger():
 		# maybe loss or something
 		pass
 
-	def after_epoch(self):
+	def after_epoch(self, train_loss, test_loss) -> None:
 		""" Update values after epoch """
 
 		self.epochs_trained += 1
 
-	def plot_performence(self):
-		# copy paste from homework 2
-		fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-		train_acc, train_loss = zip(*self.train_acc_loss)
-		test_acc, test_loss = zip(*self.test_acc_loss)
-		x = np.arange(1, len(train_acc) + 1)
+		self.g_train_loss.append(train_loss[0])
+		self.d_train_loss.append(train_loss[1])
+		self.g_test_loss.append(test_loss[0])
+		self.d_test_loss.append(test_loss[1])
 
-		# # plotting accuracy data
-		axs[0].plot(x, train_acc, label='train accuracy')
-		axs[0].plot(x, test_acc, label='test accuracy')
-		# # Adding labels and legend
-		axs[0].set_xlabel('Global training batches performed')
-		axs[0].set_ylabel('Accuracy')
-		axs[0].set_title('Accuracy per batch')
-		axs[0].set_ylim([0,1])
-		axs[0].legend()
+	def add_images(self, real_images, fake_images) -> None:
+		for i in range(len(real_images)):
+			gray = real_images[i][0]
+			self.recent_images.append((gray, real_images[i], fake_images[i]))
 
-		# # plotting loss data
-		axs[1].plot(x, train_loss, label='train loss')
-		axs[1].plot(x, test_loss, label='test loss')
-		# # Adding labels and legend
-		axs[1].set_xlabel('Global training batches performed')
-		axs[1].set_ylabel('Loss')
-		axs[1].set_title('Loss per batch')
-		axs[1].legend()
+	def plot_performence(self, show=True) -> None:
+		x = np.arange(1, len(self.train_acc) + 1)
 
-		# Adjust layout
-		plt.tight_layout()
-		# Display the plot
-		plt.show()
+		plt.subplot(121)
+		plt.plot(x, self.g_train_loss, color='c', label='Train loss')
+		plt.plot(x, self.g_test_loss, color='g', label='Test loss')
+		plt.xlabel('Epoch')
+		plt.ylabel('Average Loss')
+		plt.title('Generator Loss')
+		plt.legend()
+
+		plt.subplot(122)
+		plt.plot(x, self.d_train_loss, color='c', label='Train loss')
+		plt.plot(x, self.d_test_loss, color='g', label='Test loss')
+		plt.xlabel('Epoch')
+		plt.ylabel('Average Loss')
+		plt.title('Discriminator Loss')
+		plt.legend()
+
+		if show: plt.show()
+		else:
+			model_path = os.path.join(SAVE_PATH, self.name)
+			plt.savefig(os.path.join(model_path, 'performence.png'))
+			plt.clf()
+
+	def plot_coloring(self, show=True) -> None:
+
+		img_count = len(self.recent_images)
+		if img_count == 0: return
+
+		for i in range(img_count):
+			gray, real, fake = self.recent_images[i]
+
+			plt.subplot(img_count,3,(i*3)+1)
+			if i == 0: plt.title("Grayscale")
+			plt.axis('off')
+			plt.imshow(gray_tensor_to_image(gray), cmap='gray')
+
+			plt.subplot(img_count,3,(i*3)+2)
+			if i == 0: plt.title("Real Color")
+			plt.axis('off')
+			plt.imshow(tensor_to_image(real))
+
+			plt.subplot(img_count,3,(i*3)+3)
+			if i == 0: plt.title("Fake Color")
+			plt.axis('off')
+			plt.imshow(tensor_to_image(fake))
+
+		if show: plt.show()
+		else:
+			model_path = os.path.join(SAVE_PATH, self.name)
+			plt.savefig(os.path.join(model_path, 'coloring.png'))
+			plt.clf()
 
