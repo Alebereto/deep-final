8f53999c1d024e45bfecd26430ee58416ff18e0d
Albert Khayat <1albertkhayat1@gmail.com>
2024-04-21 21:29:17 +0300

    Remove comment

--- data/ImagesDataset.py
+++ data/ImagesDataset.py
@@ -38,12 +38,6 @@ class ImagesDataset(Dataset):
 		ab = img[[1,2],...] / 110.	# Get normalized ab channels (value range is (-107.8573, 100))
 
 		return l, ab
-
-	# def copy_dataset(self, dest:str):
-	# 	""" copies all images in dataset to dest location """
-
-	# 	for i, path in enumerate(self.paths):
-	# 		copyfile(path, f'{dest}\\{i+1}.jpg')
 	
 	def print_stats(self):
 		print('=====Dataset Stats=====')
